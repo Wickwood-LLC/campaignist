0ac2f95a906f3dcc0f96dc267f74e0246d3052df
Junaid PV <junaid@detlus.com>
2020-05-10 06:49:42 +0530

    Remove link for downloading message.em file.

--- js/send-mail.js
+++ js/send-mail.js
@@ -4,32 +4,17 @@
  */
 
 (function($, Drupal) {
-  var textFile = null,
-  makeTextFile = function (text) {
-    var data = new Blob([text], {type: 'text/plain'});
-    if (textFile !== null) {
-      window.URL.revokeObjectURL(textFile);
-    }
-    textFile = window.URL.createObjectURL(data);
-    return textFile;
-  };
-
   Drupal.behaviors.fullcalendarView = {
     attach: function(context, settings) {
       $('.campaignist-send-mail-button', context).click(function(){
         $form = $(this).closest("form");
         let recipient = $form.find('input[name="recipient"]').val();
-        let subject = $form.find('input[name="subject"]').val();
+        let subject = $form.find('input[name="subject"]').val().replace(/ /gm,"%20");
         // .replace(/(\r\n|\n|\r)/gm,"%0D%0A")
         let body = $form.find('input[name="body"]').val().replace(/ /gm,"%20");
         let mail = $form.find('textarea[name="mail"]').val();
-        $form.attr('action', 'mailto:' + recipient + '?subject=' + subject + '&body=' + body);
+        $form.attr('action', 'mailto:' + encodeURIComponent(recipient) + '?subject=' + encodeURIComponent(subject));
         $form.submit();
-
-        $download_link = $(this).siblings('.download-link');
-        $download_link.attr('href', makeTextFile(mail));
-        $download_link.show();
-        $download_link.trigger( "click" );
       });
 
       var clipboard = new ClipboardJS('.campaignist-textfield-copier .textfield-copier-button', {
